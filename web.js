94bf1b73bdf317b09b323d4a7e3110d625a033a9
danielosbaldo <daniel@localhost.localdomain>
2015-06-10 16:35:10 -0700

    fix webjs

--- web.js
+++ web.js
@@ -3,6 +3,6 @@ var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
-app.user(morgan('dev'));
+app.use(morgan('dev'));
 app.use(gzippo.staticGzip(""+_dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
